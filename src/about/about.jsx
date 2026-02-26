7dbf558981e5744e28838c937aec34ee5d6eb06e
Crystal Hwang <clhwang@byu.edu>
2026-02-26 15:18:55 -0700

    updates

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import './about.css';
 
-export function About(props) {
-  const [imageUrl, setImageUrl] = React.useState('');
-  const [quote, setQuote] = React.useState('Loading...');
-  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
-
-  React.useEffect(() => {
-    setImageUrl(`placeholder.jpg`);
-    setQuote('Show me the code');
-    setQuoteAuthor('Linus Torvalds');
-  }, []);
-  
+export function About() {
   return (
     <main className="container-fluid bg-secondary text-center">
       <div>
